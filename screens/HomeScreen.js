deecba42fee70877821a06049a228d9ff2a85abc
George GIO <A01653261@itesm.mx>
2022-08-19 19:54:00 -0500

    Creation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,37 @@
+import { View, Text,SafeAreaView, Image } from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [])
+
+  return (
+    <SafeAreaView className = 'bg-white pt-5'>
+      <Text className = 'text-red-500 mt-6'>
+        {/*Header*/}
+        <View className ='flex-row pb-3 items-center mx-4 space-x-2'>
+          <Image
+          source = {{
+            uri: 'http://links.papareact.com/wru'
+          }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+          />
+          <View>
+            <Text className='font-bold text-gray-400 text-xs'>
+              Deliver Now
+            </Text>
+            <Text className= 'font bold text-xl'>
+              Current Location
+            </Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+}
